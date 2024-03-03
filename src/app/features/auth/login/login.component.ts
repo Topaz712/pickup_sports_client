4452f50c86e1d398d9746291620836499ab43463
Topaz712 <tzam712@gmail.com>
2024-03-03 02:02:53 -0600

    FIXED: login/logout bugs

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -35,8 +35,6 @@ export class LoginComponent {
 
       this.authService.login(username, password).subscribe({
         next: (res: any) => {
-          console.log(res);
-          this.authService.setToken(res.token);
           this.router.navigate(['/']);
         },
         error: (error: any) => {
--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -31,8 +31,11 @@ export class NavigationComponent {
   }
 
   logout() {
-    this.toggleSidebar();
+    if (this.isSidebarVisible) {
+      this.toggleSidebar();
+    }
     this.authService.logout();
+    this.userService.setCurrentUser(null);
   }
 
   toggleSidebar() {
