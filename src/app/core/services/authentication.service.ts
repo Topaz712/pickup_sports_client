527f9497c88a413ce1a70cc729f44887d77dceb6
Topaz712 <tzam712@gmail.com>
2024-03-03 16:12:39 -0600

    ADDED: Sign up form

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -29,6 +29,10 @@ export class AuthenticationService {
       );
   }
 
+  signup(data: any) {
+    return this.http.post(`${environment.apiUrl}/users`, data);
+  }
+
   setToken(token: string) {
     localStorage.setItem('token', token);
   }
